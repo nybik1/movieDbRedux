36ec482655a7f1bfdedc70c675d7a5262af2709d
nybik1 <spudegbutman@gmail.com>
2020-07-17 17:29:55 +0300

    Updates

---
+++ precache-manifest.f5837db54025ee8bdeca565ddf8d50b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222e44123402dbf3281f3ddac7c9cf4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405166a04cc33fcb39bb",
+    "url": "/static/css/2.4b39fc77.chunk.css"
+  },
+  {
+    "revision": "3719792803ea27acaac7",
+    "url": "/static/css/main.3ce03dc5.chunk.css"
+  },
+  {
+    "revision": "405166a04cc33fcb39bb",
+    "url": "/static/js/2.55884995.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/static/js/2.55884995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3719792803ea27acaac7",
+    "url": "/static/js/main.bbb7918b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f5c3cdf5d48acdde238dd23569c9397e",
+    "url": "/static/media/back.f5c3cdf5.svg"
+  }
+]);
